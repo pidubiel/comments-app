7be1ffbcf159d0a682733e9758545f8617382851
Piotr Dubiel <piotrdubiel@icloud.com>
2020-01-23 23:28:42 +0100

    Added styles to single component

--- src/components/SingleComment/index.js
+++ src/components/SingleComment/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
+import { SingleCommentContainer } from '../../styledComponents';
+import { connect } from 'react-redux';
+import { addToFav, removeFromFav } from './../../redux/comments/commentsActions';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'redux';
 
-const SingleComment = () => {
-  return <p>SingleComment</p>;
+const Title = styled.h3`
+  font-size: 20px;
+  text-align: center;
+  color: #7a7a8c;
+  font-weight: 900;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #0e0c12;
+  padding-bottom: 25px;
+`;
+const Email = styled.p`
+  color: #fff;
+  font-weight: 200;
+  font-size: 10px;
+`;
+const Content = styled.p`
+  font-weight: 200;
+  margin: 20px 0;
+  color: #fff;
+  padding-bottom: 30px;
+  margin-bottom: auto;
+`;
+
+const SingleComment = ({ data, addToFav, removeFromFav, match }) => {
+  const isInFavPath = match.path === '/favourites';
+  const renderButton = () => {
+    if (data.isFav) {
+      if (isInFavPath) {
+        return <button onClick={() => removeFromFav(data)}>Usuń z ulubionych</button>;
+      } else {
+        return <h5>Polubiono</h5>;
+      }
+    } else {
+      return <button onClick={() => addToFav(data)}>Dodaj do ulubionych</button>;
+    }
+  };
+  return (
+    <SingleCommentContainer>
+      <Title>{data.title}</Title>
+      <Email>{data.email}</Email>
+      <Content>{data.content.slice(0, 20)}...</Content>
+      {renderButton()}
+    </SingleCommentContainer>
+  );
 };
 
-export default SingleComment;
+const mapDispatchToProps = dispatch => {
+  return {
+    addToFav: comment => dispatch(addToFav(comment)),
+    removeFromFav: comment => dispatch(removeFromFav(comment))
+  };
+};
+
+export default compose(withRouter, connect(null, mapDispatchToProps))(SingleComment);
--- src/styledComponents/index.js
+++ src/styledComponents/index.js
@@ -1,10 +1,10 @@
 import GlobalStyles from './globalStyles';
 import MainContainer from './mainContainer';
 import NavigationList from './navigationList';
-import SingleCommentStyles from './singleCommentStyles';
+import SingleCommentContainer from './singleCommentStyles';
 import CommentListStyles from './commentListStyles';
 import AddCommentContainer from './addCommentContainer';
 import StyledFormGroup from './styledFormGroup';
 import CommentListContainer from './commentListContainer';
 
-export { GlobalStyles, MainContainer, NavigationList, SingleCommentStyles, CommentListStyles, AddCommentContainer, StyledFormGroup, CommentListContainer };
+export { GlobalStyles, MainContainer, NavigationList, SingleCommentContainer, CommentListStyles, AddCommentContainer, StyledFormGroup, CommentListContainer };
